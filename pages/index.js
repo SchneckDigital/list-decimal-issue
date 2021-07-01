ad86d9c2dc313ba3cff629560857307378473405
Lloyd Schneck <lloydschneck@Lloyds-MacBook-Pro.local>
2021-07-01 11:55:12 +0930

    list-decimal initial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function IndexPage() {
+  return (
+    <div>
+      <ol className="list-decimal">
+        <li>hello</li>
+        <li>hello</li>
+      </ol>
+    </div>
+  );
+}
